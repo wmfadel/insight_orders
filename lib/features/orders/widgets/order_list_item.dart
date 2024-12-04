951f1cd066518158f76faa4f7df69379819670bb
wmfadel2 <m.fadel@awaed.co>
2024-12-04 17:13:23 +0200

    refactor: Improve OrderListItem layout and information display
    Redesigned the `OrderListItem` layout to be more compact and informative.
    
    - Changed the main layout
     from a `Row` to a `Column` for better organization.
    - Moved the company, buyer, and status information to the top row for better visibility.
    - Added a new row to display order price and registration date using the new `_ItemData` widget.
    - Placed the tags below the divider in
     a scrollable row.
    - Updated image container to have circular border radius.
    - Improved widget spacing for better visual balance.

--- lib/features/orders/widgets/order_list_item.dart
+++ lib/features/orders/widgets/order_list_item.dart
@@ -18,144 +18,111 @@ class OrderListItem extends StatelessWidget {
       elevation: 2,
       child: Padding(
         padding: const EdgeInsets.all(16),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            // Image Section
-            Semantics(
-              label:
-                  '${context.translate(L10nKeys.orderImageFor)} ${order.company ?? context.translate(L10nKeys.unknown)}',
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.network(
-                  order.picture ?? '',
-                  width: 40,
-                  height: 40,
-                  fit: BoxFit.cover,
-                  errorBuilder: (context, error, stackTrace) =>
-                      const Icon(Icons.broken_image_outlined),
+            Row(
+              children: [
+                // Image Section
+                Semantics(
+                  label:
+                      '${context.translate(L10nKeys.orderImageFor)} ${order.company ?? context.translate(L10nKeys.unknown)}',
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                      child: Image.network(
+                        order.picture ?? '',
+                        width: 40,
+                        height: 40,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) =>
+                            const Icon(Icons.broken_image_outlined),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+                const SizedBox(width: 16),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      order.company ?? '-',
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelLarge
+                          ?.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '${context.translate(L10nKeys.by)}: ${order.buyer ?? '-'}',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 8),
+                const Spacer(),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                  decoration: BoxDecoration(
+                    color: _statusColor(context, order.status).withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Semantics(
+                    label:
+                        '${context.translate(L10nKeys.orderStatus)}: ${order.status?.name ?? "-"}',
+                    child: Text(
+                      order.status?.name ?? '-',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodySmall
+                          ?.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(width: 16),
-
-            // Content Section
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Company and Buyer Name
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        order.company ?? '-',
-                        style: Theme.of(context)
-                            .textTheme
-                            .labelLarge
-                            ?.copyWith(fontWeight: FontWeight.bold),
-                      ),
-                      Semantics(
-                        label:
-                            '${context.translate(L10nKeys.price)}: ${order.price ?? "-"}',
-                        child: Text(
-                          order.price ?? '-',
-                          style:
-                              Theme.of(context).textTheme.bodyMedium?.copyWith(
-                                    fontWeight: FontWeight.bold,
-                                    color: _statusColor(context, order.status),
-                                  ),
-                        ),
-                      ),
-                    ],
+            const Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _ItemData(
+                    label: context.translate(L10nKeys.price),
+                    value: order.price ?? '-'),
+                _ItemData(
+                    label: context.translate(L10nKeys.registered),
+                    value: order.registered?.split('T')[0] ?? '-'),
+                // _ItemData(
+                //     label:'Active',// context.translate(L10nKeys.registered),
+                //     value:order.isActive==true?'Yes':'No'),
+              ],
+            ),
+            const SizedBox(height: 8),
+            Row(
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: (order.tags ?? [])
+                          .map((tag) => Container(
+                                padding: const EdgeInsets.all(4),
+                                margin: const EdgeInsets.only(right: 8),
+                                decoration: BoxDecoration(
+                                    color: AppColors.primary.withOpacity(0.25),
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(4))),
+                                child: Text(tag),
+                              ))
+                          .toList(),
+                    ),
                   ),
-                  const SizedBox(height: 4),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '${context.translate(L10nKeys.by)}: ${order.buyer ?? '-'}',
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 8, vertical: 4),
-                        decoration: BoxDecoration(
-                          color: _statusColor(context, order.status)
-                              .withOpacity(0.3),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Semantics(
-                          label:
-                              '${context.translate(L10nKeys.orderStatus)}: ${order.status?.name ?? "-"}',
-                          child: Text(
-                            order.status?.name ?? '-',
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodySmall
-                                ?.copyWith(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 8),
-
-                  // Status & Tags
-                  Row(
-                    children: [
-                      Expanded(
-                        child: SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                            children: (order.tags ?? [])
-                                .map((tag) => Container(
-                                      padding: const EdgeInsets.all(4),
-                                      margin: const EdgeInsets.only(right: 8),
-                                      decoration: BoxDecoration(
-                                          color: AppColors.primary
-                                              .withOpacity(0.25),
-                                          borderRadius: const BorderRadius.all(
-                                              Radius.circular(4))),
-                                      child: Text(tag),
-                                    ))
-                                .toList(),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 8),
-
-                  // Registration Date and Active Status
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '${context.translate(L10nKeys.registered)}: ${order.registered?.split('T')[0]}',
-                        style: Theme.of(context)
-                            .textTheme
-                            .labelMedium
-                            ?.copyWith(color: AppColors.onSurface),
-                      ),
-                      Semantics(
-                        label: context.translate((order.isActive ?? false)
-                            ? L10nKeys.activeOrder
-                            : L10nKeys.inactiveOrder),
-                        child: Icon(
-                          (order.isActive ?? false)
-                              ? Icons.check_circle
-                              : Icons.cancel,
-                          color: (order.isActive ?? false)
-                              ? Colors.green
-                              : Colors.red,
-                          size: 20,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
@@ -169,5 +136,32 @@ class OrderListItem extends StatelessWidget {
       OrderStatus.delivered => AppColors.statusDelivered,
       OrderStatus.returned => AppColors.statusReturned
     };
+  }
+}
+
+class _ItemData extends StatelessWidget {
+  final String label;
+  final String value;
+
+  const _ItemData({required this.label, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(label,
+            style: Theme.of(context)
+                .textTheme
+                .labelMedium
+                ?.copyWith(color: Colors.grey)),
+        Text(value,
+            style: Theme.of(context)
+                .textTheme
+                .bodyMedium
+                ?.copyWith(fontWeight: FontWeight.bold)),
+      ],
+    );
   }
 }
